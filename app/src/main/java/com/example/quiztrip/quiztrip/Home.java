81fc5628ca12e00bb05ca18c6358d40ddb640255
Aluno <Aluno@Digital.net>
2017-08-18 22:11:06 -0300

    commit 18_08_17 tela splash

---
+++ app/src/main/java/com/example/quiztrip/quiztrip/Home.java
@@ -0,0 +1,28 @@
+package com.example.quiztrip.quiztrip;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class Home extends AppCompatActivity {
+
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+//        btn1 = (Button)findViewById(R.id.btnIniciar1);
+//        btn1.setLayoutParams(new ConstraintLayout.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+    }
+
+
+}
